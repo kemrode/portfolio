5ac4690bc37055e040f2ab2f25454d0fef8d0dac
kevin_fichou <kevinfichou@gmail.com>
2023-11-16 13:34:28 +0100

    feat(0019):Créer la page Projets
    feat(0019): make the project cell layout

---
+++ src/assets/infos/infoText.tsx
@@ -0,0 +1,3 @@
+export const projectParagraph = () => {
+    return "Bienvenue dans mon blocs PROJETS ! Ici, vous découvrirez mes diverses réalisations et travaux, depuis 2020, année lors de laquelle j’ai pu découvrir le développement informatique.SWIFT, C#, Unity, Blender...Projets personnels, comme projets scolaires, cliquez sur une vignette pour avoir plus d’informations à son propos";
+}
